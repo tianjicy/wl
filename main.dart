c58c07d7e31fb54ed7f1a3d0a59499bc1ee93def
tianjicy <113495620+tianjicy@users.noreply.github.com>
2023-03-13 11:00:33 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,4 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,123 +6,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Video Player',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Video Player'),
+      title: 'My Flutter App',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  VideoPlayerController _controller;
-  Future<void> _initializeVideoPlayerFuture;
-  bool _isPlaying = false;
-  final FlutterFFmpeg _flutterFFmpeg = FlutterFFmpeg();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.network(
-        'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4');
-    _initializeVideoPlayerFuture = _controller.initialize();
-    _controller.addListener(() {
-      setState(() {
-        _isPlaying = _controller.value.isPlaying;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  Future<String> convertVideo(String inputPath, String outputPath) async {
-    final arguments = '-i $inputPath -c:v libx264 -c:a copy $outputPath';
-    await _flutterFFmpeg.execute(arguments);
-    return outputPath;
-  }
-
-  void _playVideo() {
-    setState(() {
-      _isPlaying = true;
-      _controller.play();
-    });
-  }
-
-  void _pauseVideo() {
-    setState(() {
-      _isPlaying = false;
-      _controller.pause();
-    });
-  }
-
-  void _stopVideo() {
-    setState(() {
-      _isPlaying = false;
-      _controller.pause();
-      _controller.seekTo(Duration.zero);
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('My Home Page'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FutureBuilder(
-              future: _initializeVideoPlayerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                labelText: 'Name',
+              ),
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-                  onPressed: _isPlaying ? _pauseVideo : _playVideo,
-                ),
-                SizedBox(width: 20),
-                IconButton(
-                  icon: Icon(Icons.stop),
-                  onPressed: _stopVideo,
-                ),
-              ],
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Button Pressed'),
+                      content: Text('Youthe button!'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('OK'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Press Me'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          String inputPath = 'path/to/input/video.avi';
-          String outputPath = 'path/to/output/video.mp4';
-          String convertedPath = await convertVideo(inputPath, outputPath);
-
-          setState(() {
-            _controller = VideoPlayerController.file(File(convertedPath
+    );
+  }
+}
